dc115f93c4af6bcb65bb63df805d42d63e0d0332
Raghav Tandon <raghavtan@gmail.com>
2018-12-26 13:26:34 +0530

    cpython init file

---
+++ compile.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+from Cython.Build import cythonize
+ext_modules = [
+    Extension("mymodule1",  ["mymodule1.py"]),
+    Extension("mymodule2",  ["mymodule2.py"]),
+    #   ... all your modules that need be compiled ...
+]
+setup(
+    name = 'My Program Name',
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = cythonize()
+)
--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import json
 from src.server import Server
 from utilities import Config
 
-if __name__ == '__main__':
+
+def main():
     with open('config.json') as f:
         config = Config(json.load(f))
         server = Server(config)
         server.load_routes()
         server.start()
+
+
+if __name__ == '__main__':
+    main()
